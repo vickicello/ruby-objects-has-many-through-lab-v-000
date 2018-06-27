3055f8315b9a5a0aabdb429a3f0d07e5f4cc6425
Vicki Hamm <vicki.cello@gmail.com>
2018-06-27 19:38:35 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,15 +1,22 @@
 class Genre
 
-  attr_accessor :name
+  attr_accessor :name, :songs
 
   @@all = []
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
   def self.all
     @@all
+  end
+
+  def new_song(name, artist)
+    song = Song.new(name, artist)
+    @songs << song
+    song.Genre = self
   end
 
 end
