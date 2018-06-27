2c242a0aa81ed13b33c66d0d50fd0f6663d9bd7f
Vicki Hamm <vicki.cello@gmail.com>
2018-06-27 19:56:01 +0000

    automatically backed up by learn

---
+++ lib/patient.rb
@@ -0,0 +1,19 @@
+class Patient
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(doctor, date)
+    appointment = Appointment.new(doctor, date, self)
+  end
+
+  
